4dd099c3871105738165a2269f6fc0a32ea8c01a
Johan Quiroga <johan.c.quiroga@gmail.com>
2020-08-07 22:16:27 -0500

    feat: add environment variables config

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,7 @@ export default function Home({ allPostsData }: { allPostsData: { title: string; 
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <p>Environment: {process.env.NEXT_PUBLIC_API_URL}</p>
         <p>[Your self introduction]</p>
         <p>
           (This is a sample website - you'll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>)
