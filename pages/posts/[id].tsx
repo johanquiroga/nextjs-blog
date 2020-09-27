b534a1a1cfa0cf79bab839db86ff50822d814a3c
Johan Quiroga <johan.c.quiroga@gmail.com>
2020-09-27 16:36:09 -0500

    fix: typing error

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -17,7 +17,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getPostData(params.id as string);
+  const postData = await getPostData(params?.id as string);
   return {
     props: {
       postData,
